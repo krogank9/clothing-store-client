b6d623562de6c8d79e9836e3195d1a3633275cb9
krogank9 <wifi.mouse.app@gmail.com>
2020-01-05 17:15:59 -0500

    fix production api url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   //API_ENDPOINT: 'http://localhost:8000/api',
   //API_ENDPOINT: 'https://boiling-chamber-32694.herokuapp.com/api',
-  API_ENDPOINT: window.location.hostname === "localhost"? 'http://localhost:8000/api' : 'https://peaceful-caverns-70329.herokuapp.com/',
+  API_ENDPOINT: window.location.hostname === "localhost"? 'http://localhost:8000/api' : 'https://peaceful-caverns-70329.herokuapp.com/api',
   TOKEN_KEY: 'clothing-store-client-auth-token',
   CART_KEY: 'clothing-store-client-cart',
 }
