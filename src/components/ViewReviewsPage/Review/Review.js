4d32335d28eb5d896e1f449c6c12a563f024e6ae
krogank9 <wifi.mouse.app@gmail.com>
2020-01-05 23:25:22 -0500

    fix review text

--- src/components/ViewReviewsPage/Review/Review.js
+++ src/components/ViewReviewsPage/Review/Review.js
@@ -9,9 +9,10 @@ class Review extends Component {
     return (
       <li className="review-li">
         <div>
-          {this.props.userName}
-          {this.props.headline}
-          {Utils.getRatingText(this.props.rating)}
+          <div>
+            {this.props.userName} says: {this.props.headline}
+          </div>
+          <div>{Utils.getRatingText(this.props.rating)}</div>
           <div>{this.props.content}</div>
         </div>
       </li>
